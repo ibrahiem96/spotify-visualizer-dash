45bd74cec8b3d1f1cff6e5661f20cbbb5a6b1966
Ibrahiem <ibrahiem.mohammad@fitchratings.com>
2020-03-29 17:19:18 -0500

    initial

---
+++ public/client-track-features.js
@@ -0,0 +1,39 @@
+// client-side js
+// run by the browser each time your view template is loaded
+
+$(function() {
+
+  $('form').submit(function(event) {
+    event.preventDefault();
+    
+    $('#track-features').empty();
+    let artist = $('.form-control').val();
+    // alert(country);
+
+    $.get('/track_features?'+$.param({artist: artist}), function(artist_tracks) {
+
+
+    	let div = $('<h1>hello</h1>');
+    	// show the track information
+
+    });
+
+    // Send a request to our backend (server.py) to get new releases for the currently selected country
+    // $.get('/new_releases?' + $.param({country: country}), function(new_releases) {
+      
+    //   // Loop through each album in the list
+    //   new_releases.albums.items.forEach(function(release) {
+        
+    //     // Use the returned information in the HTML
+    //     let div = $('<div class="sp-entity-container"><a href="' + release.external_urls.spotify + 
+    //             '"><div style="background:url(\'' + release.images[0].url + 
+    //             '\')" class="sp-cover" alt="Album cover"></div></a><h3 class="sp-title">' + release.name + 
+    //             '</h3><p class="text-grey-55 sp-by">By ' + release.artists[0].name + '</p></div>')
+        
+    //     div.appendTo('#new-releases')
+        
+    //   });
+    // });
+  });
+
+});
---
+++ server.py
@@ -0,0 +1,87 @@
+from __future__ import print_function    # (at top of module)
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+
+from flask import Flask, request, render_template, jsonify
+
+import json
+import time
+import sys
+import os
+
+# Authenticate with Spotify using the Client Credentials flow
+client_credentials_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+app = Flask(__name__, static_folder='public', template_folder='views')
+
+@app.route('/')
+def homepage():
+    # Displays homepage
+    # return render_template('index.html')
+    return render_template('track-feature.html')
+  
+@app.route('/new_releases', methods=['GET'])
+def new_releases():
+  
+    # Use the country from the query parameters, if provided
+    if 'country' in request.args:
+        country = request.args['country']
+    else:
+        country = 'SE'
+    
+    # Send request to the Spotify API
+    new_releases = sp.new_releases(country=country, limit=5, offset=0)
+    
+    # Return the list of new releases
+    return jsonify(new_releases)
+
+# TODO: complete this method:
+#       - provide track info on each
+@app.route('/track_features', methods=['GET'])
+def track_features():
+
+    # Use the country from the query parameters, if provided
+    if 'artist' in request.args:
+        artist = request.args['artist']
+    else:
+        artist = 'Drake'
+
+    print(artist)
+
+    results = sp.search(q=artist, limit=5, offset=0)
+    features = []
+    data = []
+    for idx, track in enumerate(results['tracks']['items']):
+        print(idx+1, track['name'], track['id'])
+        features += sp.audio_features(str(track['id']))
+        data.append({'artist': artist, track['name']: features})
+        # print(json.dumps(features, indent=4)) 
+
+    return jsonify(data)
+
+
+# if len(sys.argv) > 1:
+#     tids = sys.argv[1:]
+#     print(tids)   
+
+#     if 'debug' in tids:
+#     	sp.trace = True
+
+#     start = time.time()
+    
+#     results = sp.search(q=tids, limit=5)
+#     for idx, track in enumerate(results['tracks']['items']):
+#         print(idx, track['name'], track['id'])
+#         features = sp.audio_features(str(track['id']))
+#         print(json.dumps(features, indent=4))
+
+#     delta = time.time() - start
+#     # print(json.dumps(features, indent=4))
+#     print("features retrieved in %.2f seconds" % (delta,))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
+
